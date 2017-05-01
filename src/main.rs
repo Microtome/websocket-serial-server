764619dec6c011db8263bd6bb1ab9d53b3e6048a
daniel <daniel@vizualiiz.com>
2017-05-01 10:36:44 -0700

    For now, echoing back request to client, show json parsing and error handling is working

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ mod serial_support;
 
 use std::thread;
 use std::io::Write;
-use std::time::Duration;
 use std::collections::HashMap;
 
 use argparse::{ArgumentParser, Store};
@@ -23,146 +22,150 @@ use hyper::Server as HttpServer;
 use hyper::net::Fresh;
 use hyper::server::request::Request;
 use hyper::server::response::Response;
-use serialport as sp;
 
 use serial_support::messages::*;
 use serial_support::common::*;
 
 fn main() {
 
-    // Init logger
-    env_logger::init();
+  // Init logger
+  env_logger::init().unwrap();
 
-    let spSettings = sp::SerialPortSettings {
-        baud_rate: sp::BaudRate::Baud115200,
-        data_bits: sp::DataBits::Eight,
-        flow_control: sp::FlowControl::None,
-        parity: sp::Parity::None,
-        stop_bits: sp::StopBits::One,
-        timeout: Duration::from_millis(1),
-    };
+  // let sr = SerialResponse::Response {
+  //     data: "HEY YOU".to_string(),
+  //     base64: None,
+  // };
+  // info!("serialized = {}", serde_json::to_string(&sr).unwrap());
 
+  // Default port number
+  let mut port = 8080;
+  // Parse cmdline args
+  {
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Provide access to serial ports over JSON Websockets");
+    ap.refer(&mut port)
+      .add_option(&["-p", "--port"], Store, "Http Port");
+    ap.parse_args_or_exit();
+  }
 
-    // let sr = SerialResponse::Response {
-    //     data: "HEY YOU".to_string(),
-    //     base64: None,
-    // };
-    // info!("serialized = {}", serde_json::to_string(&sr).unwrap());
+  // The HTTP server handler
+  let http_handler = move |_: Request, response: Response<Fresh>| {
+    let mut response = response.start().unwrap();
+    // Send a client webpage
+    response
+      .write_all(format!(include_str!("websockets.html"), wsPort = port + 1).as_bytes())
+      .unwrap();
+    response.end().unwrap();
+  };
 
-    // Default port number
-    let mut port = 8080;
-    // Parse cmdline args
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("Provide access to serial ports over JSON Websockets");
-        ap.refer(&mut port)
-            .add_option(&["-p", "--port"], Store, "Http Port");
-        ap.parse_args_or_exit();
-    }
+  info!("Using ports {} {}", port, port + 1);
 
-    // The HTTP server handler
-    let http_handler = move |_: Request, response: Response<Fresh>| {
-        let mut response = response.start().unwrap();
-        // Send a client webpage
-        response
-            .write_all(format!(include_str!("websockets.html"), wsPort = port + 1).as_bytes())
-            .unwrap();
-        response.end().unwrap();
-    };
+  // Start listening for http connections
+  thread::spawn(move || {
+                  let http_server = HttpServer::http(format!("127.0.0.1:{}", port)).unwrap();
+                  http_server.handle(http_handler).unwrap();
+                });
 
-    info!("Using ports {} {}", port, port + 1);
+  // Start listening for WebSocket connections
+  let ws_server = Server::bind(format!("127.0.0.1:{}", port + 1)).unwrap();
 
-    // Start listening for http connections
+  for connection in ws_server.filter_map(Result::ok) {
+    // Spawn a new thread for each connection.
     thread::spawn(move || {
-                      let http_server = HttpServer::http(format!("127.0.0.1:{}", port)).unwrap();
-                      http_server.handle(http_handler).unwrap();
-                  });
+      if !connection
+            .protocols()
+            .contains(&"rust-websocket".to_string()) {
+        connection.reject().unwrap();
+        return;
+      }
 
-    // Start listening for WebSocket connections
-    let ws_server = Server::bind(format!("127.0.0.1:{}", port + 1)).unwrap();
+      let mut client = connection
+        .use_protocol("rust-websocket")
+        .accept()
+        .unwrap();
 
-    for connection in ws_server.filter_map(Result::ok) {
-        // Spawn a new thread for each connection.
-        thread::spawn(move || {
-            if !connection
-                    .protocols()
-                    .contains(&"rust-websocket".to_string()) {
-                connection.reject().unwrap();
-                return;
-            }
+      let ip = client.peer_addr().unwrap();
 
-            let mut client = connection
-                .use_protocol("rust-websocket")
-                .accept()
-                .unwrap();
+      info!("Connection from {}", ip);
 
-            let ip = client.peer_addr().unwrap();
+    //   let message = Message::text("Hello".to_string());
+    //   client.send_message(&message).unwrap();
 
-            info!("Connection from {}", ip);
+      let (mut receiver, mut sender) = client.split().unwrap();
 
-            let message = Message::text("Hello".to_string());
-            client.send_message(&message).unwrap();
+      for message in receiver.incoming_messages() {
+        let message: Message = message.unwrap();
 
-            let (mut receiver, mut sender) = client.split().unwrap();
+        match message.opcode {
 
-            for message in receiver.incoming_messages() {
-                let message: Message = message.unwrap();
+          Type::Close => {
+            let message = Message::close();
+            sender
+              .send_message(&message)
+              .unwrap_or_else(|_| info!("Client {} hung up!", ip));
+            info!("Client {} disconnected", ip);
+            return;
+          }
 
-                match message.opcode {
-                    Type::Close => {
-                        let message = Message::close();
-                        sender
-                            .send_message(&message)
-                            .unwrap_or_else(|_| info!("Client {} hung up!", ip));
-                        info!("Client {} disconnected", ip);
-                        return;
-                    }
-                    Type::Ping => {
-                        let message = Message::pong(message.payload);
-                        sender
-                            .send_message(&message)
-                            .unwrap_or_else(|_| info!("Could not ping client {}!", ip));
-                    }
-                    _ => {
-                        let msg = String::from_utf8_lossy(&message.payload);
-                        // Unwrap to error;
-                        let serialReq: SerialRequest = serde_json::from_str(&msg).unwrap();
+          Type::Ping => {
+            let message = Message::pong(message.payload);
+            sender
+              .send_message(&message)
+              .unwrap_or_else(|_| info!("Could not ping client {}!", ip));
+          }
 
-                        sender
-                            .send_message(&message)
-                            .unwrap_or_else(|_| {
-                                                info!("Could send message {} to client {}",
-                                                      String::from_utf8_lossy(&message.payload),
-                                                      ip)
-                                            })
-                    }
-                }
-            }
-        });
-    }
+          _ => {
+
+            // Get the payload, in a lossy manner
+            let msg = String::from_utf8_lossy(&message.payload);
+
+            // Function to remap Json Deserizilation error
+            // to a SerialResponse:Error
+            let remap_error = |_| -> SerialResponse {
+              SerialResponse::Error {
+                kind: ErrorType::JsonParseFailure,
+                msg: "Unable to parse json".to_string(),
+                detail: Some(msg.to_string()),
+                port: None,
+                handle: None,
+              }
+            };
+
+            // So we will get a result <SerialRequest::*,SerialResponse::Error> back
+            let res: Result<SerialRequest, SerialResponse> = serde_json::from_str(&msg)
+              .map_err(remap_error);
+
+            let reply = match res {
+              Ok(req) => Message::text(serde_json::to_string(&req).unwrap()),
+              Err(err) => Message::text(serde_json::to_string(&err).unwrap()),
+            };
+
+            sender
+              .send_message(&reply)
+              .unwrap_or_else(|_| {
+                                info!("Could send message {} to client {}",
+                                      String::from_utf8_lossy(&message.payload),
+                                      ip)
+                              });
+          }
+        }
+      }
+    });
+  }
 }
 
 
 
 fn handle_serial_req(req: SerialRequest) {
-    match req {
-        SerialRequest::Open { port, req_id } => {}
-        SerialRequest::Write {
-            handle,
-            port,
-            data,
-            base64,
-            req_id,
-        } => {}
-        SerialRequest::WriteLock {
-            handle,
-            port,
-            req_id,
-        } => {}
-        SerialRequest::ReleaseWriteLock {
-            handle,
-            port,
-            req_id,
-        } => {}
-    }
+  match req {
+    SerialRequest::Open { port } => {}
+    SerialRequest::Write {
+      handle,
+      port,
+      data,
+      base64,
+    } => {}
+    SerialRequest::WriteLock { handle, port } => {}
+    SerialRequest::ReleaseWriteLock { handle, port } => {}
+  }
 }
--- src/serial_support/messages.rs
+++ src/serial_support/messages.rs
@@ -1,63 +1,65 @@
+extern crate serde_json;
 
 /// Represents the valid json requests that can be made
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum SerialRequest {
-    /// Open a port for reading
-    Open { port: String, req_id: String },
-    /// Take control of a port for writing
-    WriteLock {
-        handle: String,
-        port: String,
-        req_id: String,
-    },
-    /// Release control of a port for writing
-    ReleaseWriteLock {
-        handle: String,
-        port: String,
-        req_id: String,
-    },
-    /// Write data, only works if we have control
-    Write {
-        handle: String,
-        port: String,
-        data: String,
-        base64: Option<bool>,
-        req_id: String,
-    },
+  /// Open a port for reading
+  Open { port: String },
+  /// Take control of a port for writing
+  WriteLock { handle: String, port: String },
+  /// Release control of a port for writing
+  /// If no port is given, release all write locks
+  /// for all ports
+  ReleaseWriteLock {
+    handle: String,
+    port: Option<String>,
+  },
+  /// Write data, only works if we have control
+  Write {
+    handle: String,
+    port: String,
+    data: String,
+    base64: Option<bool>,
+  },
 }
 
 /// Represents the possible error types
 /// that can be returned
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum ErrorType {
-    /// Failed to parse message
-    JsonParseFailure,
-    /// Unknown request
-    UnknownRequest,
-    /// Someone else has already locked the port for writing
-    AlreadyWriteLocked,
-    /// Error reading serial port
-    ReadError,
-    /// Error writing serial port
-    WriteError,
+  /// Failed to parse message
+  JsonParseFailure,
+  /// Unknown request
+  UnknownRequest,
+  /// Someone else has already locked the port for writing
+  AlreadyWriteLocked,
+  /// WriteLock needed
+  NeedWriteLock,
+  /// Error reading serial port
+  ReadError,
+  /// Error writing serial port
+  WriteError,
 }
 
 /// Represents the valid serial responses that can be made
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum SerialResponse {
-    /// Error response
-    Error {
-        kind: ErrorType,
-        msg: String,
-        port: Option<String>,
-        handle: Option<String>,
-    },
-    /// Data that was read from port
-    Read {
-        port: String,
-        data: String,
-        base64: Option<bool>,
-    },
-    /// Ok response showing that command was accepted
-    Ok { request: SerialRequest },
+  /// Error response
+  Error {
+    kind: ErrorType,
+    msg: String,
+    detail: Option<String>,
+    port: Option<String>,
+    handle: Option<String>,
+  },
+  /// Data that was read from port
+  Read {
+    port: String,
+    data: String,
+    base64: Option<bool>,
+  },
+  /// Ok response showing that command was accepted
+  Accepted { request: SerialRequest },
 }
+
+// json_ser_safely
