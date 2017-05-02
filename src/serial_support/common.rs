08e500a2e87f1e58eac194e7953021d1096eafdc
Daniel Joyce <daniel.a.joyce@gmail.com>
2017-05-01 22:45:16 -0700

    Rename bidirectional channel

--- src/serial_support/common.rs
+++ src/serial_support/common.rs
@@ -6,7 +6,7 @@ use std::sync::mpsc::Receiver;
 /// Returns two sender-receiver channel pairs
 /// One pair should be given to one thread
 /// and the other pair to another thread
-pub fn bichannel<T, U>() -> ((Sender<T>, Receiver<U>), (Sender<U>, Receiver<T>)) {
+pub fn bidi_channel<T, U>() -> ((Sender<T>, Receiver<U>), (Sender<U>, Receiver<T>)) {
   let side1 = channel::<T>();
   let side2 = channel::<U>();
   return ((side1.0, side2.1), (side2.0, side1.1));
